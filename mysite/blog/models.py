29608fe7ac1142b38300df7c37f8a03d3e97249a
Chris Shenton <chris@v-studios.com>
2017-07-10 16:58:33 -0400

    Image gallery, cool

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 
-from wagtail.wagtailcore.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsearch import index
 
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
+
+    def get_context(self, request):
+        # DAMN: if we name this function wrong (get_conxett), or reference the
+        # wrong context name (blog_pgs), we get NO ERROR
+        # `blogpages` is basically undeclared
+        # DAMN: this is an ugly way of getting context, why's it not just
+        # provided like pyramid?
+        context = super(BlogIndexPage, self).get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
@@ -27,5 +41,21 @@ class BlogPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('date'),
         FieldPanel('intro'),
-        FieldPanel('body', classname="full"),
+        FieldPanel('body', classname='full'),
+        InlinePanel('gallery_images', label='Gallery images')
+    ]
+
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
     ]
